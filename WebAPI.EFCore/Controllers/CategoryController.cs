dd65c87d1950a3fb5603b374ab910c4c83688a8c
Asif Saeed <asifsaeed.softexpert@gmail.com>
2023-02-10 23:53:00 +0500

    Implement Two Classes        i. ControllerClass        ii. ModelClass Implement HttpGet (Read) in Web API. Implement HttpPost (Create) in Web API. Implemnt HttpPut (Update) in Web API. Implemnt HttpDelete (Delete) in Web API.

---
+++ WebAPI.EFCore/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.EFCore.Model;
+
+namespace WebAPI.EFCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public static List<Category> listOfCategories = new List<Category>
+        {
+            new Category{Id = 1, Name = "LG", CategoryOrder = 2},
+            new Category{Id = 2, Name = "HTC", CategoryOrder = 4},
+            new Category{Id = 3, Name = "Samsung", CategoryOrder = 2},
+            new Category{Id = 4, Name = "Nokia", CategoryOrder = 4},
+            new Category{Id = 5, Name = "Vivo", CategoryOrder = 2},
+            new Category{Id = 6, Name = "Redme", CategoryOrder = 4},
+        };
+        [HttpGet]   // Reading the Code
+        public IEnumerable<Category> Get()
+        {
+            return listOfCategories;
+        }
+        [HttpPost]  // Creating the Code
+        public void Post(Category category)
+        {
+            listOfCategories.Add(category);
+        }
+        [HttpPut("{id}")]   // Updating the Code
+        public void Put(int id, Category category)
+        {
+            listOfCategories[id] = category;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            listOfCategories.RemoveAt(id);
+        }
+    }
+}
---
+++ WebAPI.EFCore/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.EFCore.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryOrder { get; set; }
+    }
+}
