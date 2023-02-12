7827b5e79c92478624b3b4765591ba306436bc05
Asif Saeed <asifsaeed.softexpert@gmail.com>
2023-02-12 21:30:23 +0500

    Implement Status Codes.

--- WebAPI.EFCore/Controllers/CategoryController1.cs
+++ WebAPI.EFCore/Controllers/CategoryController1.cs
@@ -3,7 +3,13 @@ using WebAPI.EFCore.Data;
 using WebAPI.EFCore.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+/*  Status Code
+    1xx : Informational
+    2xx : Successfuly
+    3xx : Redirection
+    4xx : Client Error
+    5xx : Server Error
+ */
 namespace WebAPI.EFCore.Controllers
 {
     [Route("api/[controller]")]
@@ -20,44 +26,50 @@ namespace WebAPI.EFCore.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Category> Get()
+        public IActionResult Get()
         {
-            return _context.categories;
+            // Not Found
+            //return NotFound();
+            //return BadRequest();
+            return Ok(_context.categories);
         }
 
         // GET api/<CategoryController1>/5
         [HttpGet("{id}")]
-        public Category Get(int id)
+        public IActionResult Get(int id)
         {
-            return _context.categories.FirstOrDefault(x=>x.Id==id);
+            return Ok(_context.categories.FirstOrDefault(x=>x.Id==id));
         }
 
         // POST api/<CategoryController1>
         [HttpPost]
-        public void Post([FromBody] Category category)
+        public IActionResult Post([FromBody] Category category)
         {
             _context.categories.Add(category);
             _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
         }
 
         // PUT api/<CategoryController1>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Category category)
+        public IActionResult Put(int id, [FromBody] Category category)
         {
-            var CategoryFromDb = _context.categories.Find(id);
+            var CategoryFromDb = _context.categories.FirstOrDefault(x => x.Id == id);
             CategoryFromDb.Name = category.Name;
             CategoryFromDb.CategoryOrder = category.CategoryOrder;
             _context.categories.Update(CategoryFromDb);
             _context.SaveChanges(); 
+            return Ok("Category Updated..!");
         }
 
         // DELETE api/<CategoryController1>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
             var CategoryFromDb = _context.categories.Find(id);
             _context.categories.Remove(CategoryFromDb);
             _context.SaveChanges();
+            return Ok("Category Deleted..!");
         }
     }
 }
